86bcd5fc5396bd10587e46c0960601898db42350
jon <xuchaoo@gmail.com>
2015-06-21 20:50:14 +0800

    去掉配置前缀,KISS

---
+++ myspider-core/src/main/java/com/oxf1/spider/TaskId.java
@@ -0,0 +1,16 @@
+package com.oxf1.spider;
+
+/**
+ * Created by cxu on 2015/6/21.
+ */
+public class TaskId {
+    private final String id;//唯一标识
+
+    public TaskId(String id){
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+}
