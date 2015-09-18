e40c24805564ac4bb3f7279417bd7356cc938655
Rain <xuchaoo@gmail.com>
2015-09-18 20:17:23 +0800

    增加内存调度的测试用例

---
+++ myspider-core/src/test/java/scheduler/MemeorySchedulerTest.java
@@ -0,0 +1,40 @@
+package scheduler;
+
+import com.oxf1.spider.TaskConfig;
+import com.oxf1.spider.exception.MySpiderException;
+import com.oxf1.spider.request.HttpRequestMethod;
+import com.oxf1.spider.request.Request;
+import com.oxf1.spider.request.impl.HttpRequest;
+import com.oxf1.spider.scheduler.Scheduler;
+import com.oxf1.spider.scheduler.impl.MemoryScheduler;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotNull;
+
+/**
+ * Created by cxu on 2015/9/18.
+ */
+public class MemeorySchedulerTest {
+    /**
+     * 插入N条，得到N条
+     */
+    @Test
+    public void test() throws MySpiderException {
+        TaskConfig taskConfig = new TaskConfig("tid", "test-task");
+        Scheduler sched = new MemoryScheduler(taskConfig);
+
+        List<Request> request = new ArrayList<Request>();
+        for(int i=0; i<100; i++){
+            request.add(new HttpRequest("http://x.com", HttpRequestMethod.HTTP_DELETE, null));
+        }
+        sched.push(request);
+
+        request = sched.poll(100);
+        assertNotNull(request);
+        assertEquals(100, request.size());
+    }
+}
